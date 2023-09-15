4ae48c470879829e67b8e9eed7c4b01c419fe573
frannyJO <144179558+frannyJO@users.noreply.github.com>
2023-09-14 23:19:59 -0500

    Update game_timer.sv

--- game_timer.sv
+++ game_timer.sv
@@ -1,3 +1,23 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/07/2023 01:33:57 PM
+// Design Name: 
+// Module Name: top_sseg
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 module game_timer(
     input logic en,
     input logic cw,
